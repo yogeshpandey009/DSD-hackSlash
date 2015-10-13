aaab9f3f2f8959bccfb9206c491482388328d391
Mihir Bhatt <mbbhatt@asu.edu>
2015-10-13 02:56:45 -0700

    UID: S2T34

--- src/com/asu/score/hackslash/engine/Database.java
+++ src/com/asu/score/hackslash/engine/Database.java
@@ -8,7 +8,7 @@ import com.asu.score.hackslash.properties.Constants;
 
 public class Database {
 	
-	public static void setConnection(final String USER, final String PASS){
+	public void setConnection(final String USER, final String PASS){
 		Connection conn = null;
 		Statement stmt = null;
 		
@@ -16,14 +16,17 @@ public class Database {
 			Class.forName("com.mysql.jdbc.Driver");
 			System.out.println("Connecting to database...");
 		    conn = DriverManager.getConnection(Constants.DB_URL,USER,PASS);
+		    
 		    System.out.println("Check!!");
+		    
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	public static void main(String[] args) {
-		setConnection("root", "1qaz2ws");
-	}
+	//public static void main(String[] args) {
+		//Database db = new Database();
+		//db.setConnection("root", "1qaz2wsx");
+	//}
 }
