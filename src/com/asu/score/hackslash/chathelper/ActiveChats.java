aeca03d238df21f74cec3cd090df05126c1e7207
bks2009 <bharat.ku.singh@gmail.com>
2015-10-22 14:00:48 -0700

    S3T58 Chat Data Helpers

---
+++ src/com/asu/score/hackslash/chathelper/ActiveChats.java
@@ -0,0 +1,13 @@
+package com.asu.score.hackslash.chathelper;
+
+import java.util.ArrayList;
+
+/**
+ * Contains all active chats for a user.
+ *
+ */
+public class ActiveChats {
+
+	public static ArrayList<Chat> activeChats = new ArrayList<Chat>();
+	
+}
---
+++ src/com/asu/score/hackslash/chathelper/Message.java
@@ -0,0 +1,43 @@
+package com.asu.score.hackslash.chathelper;
+
+/**
+ * Message class that stores the sender and the message sent.
+ *
+ */
+public class Message {
+	
+	private String sender;
+	private String msg;
+	
+	/**
+	 * Getter for Sender
+	 * @return
+	 */
+	public String getSender() {
+		return sender;
+	}
+	
+	/**
+	 * Setter for Sender
+	 * @param sender
+	 */
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	
+	/**
+	 * Getter for Message
+	 * @return
+	 */
+	public String getMsg() {
+		return msg;
+	}
+	
+	/**
+	 * Setter for message
+	 * @param msg
+	 */
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+}
