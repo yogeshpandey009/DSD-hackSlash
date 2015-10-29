b3e2b15d2bb47d83fd197f8a466284f3aeefe9a1
hmalik108 <hmalik2@asu.edu>
2015-10-29 05:14:02 -0700

    S3T64:Added query to fetch results

--- src/com/asu/score/hackslash/properties/SQLQueries.java
+++ src/com/asu/score/hackslash/properties/SQLQueries.java
@@ -3,5 +3,7 @@ package com.asu.score.hackslash.properties;
 public class SQLQueries {
 	
 	public static final String SQL_GET_USERS = "SELECT userID FROM PROFILE";
+	public static final String SQL_TASK_DETAILS = "SELECT A.USERID,A.StartDate,A.EndDate FROM ALLOCATION AS A, TASK AS T WHERE T.TASKID = A.TASKID  AND T.TASKNAME =''";
+		
 
 }
